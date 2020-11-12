2e3ac6acc1c4ff97920c7a17dca3f5bd7f3601d0
Jonathan Shaw <shaw5@unbc.ca>
2020-11-12 14:28:40 -0800

    added 'get user by id' route handler; upated postman accordingly

--- src/routers/project.js
+++ src/routers/project.js
@@ -19,4 +19,21 @@ router.post('/projects', auth, async (req, res) => {
     }
 })
 
+// Get a project by its id
+router.get('/projects/:id', async (req, res) => {
+    const _id = req.params._id
+
+    try {
+        const project = await Project.findById(_id)
+
+        if (!project) {
+            return res.status(404).send()
+        }
+
+        res.send(project)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
 module.exports = router
