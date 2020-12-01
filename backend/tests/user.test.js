fbba44adb0307e9bbc8b904095696e68fd3ec0b6
Jonathan Shaw <shaw5@unbc.ca>
2020-12-01 14:14:38 -0800

    Implemented unit testing scaffolding

---
+++ backend/tests/user.test.js
@@ -0,0 +1,36 @@
+const request = require('supertest')
+const app = require('../src/app')
+const User = require('../src/models/user')
+const { userOneId, userOne, setupDatabase } = require('./fixtures/db')
+
+beforeEach(setupDatabase)
+
+test('Should signup a new user', async () => {
+    const response = await request(app).post('/users').send({
+        name: 'Jon',
+        email: 'jon@jon.ca',
+        password: 'MyPass777!'
+    }).expect(201)
+
+    // Assert that the database was changed correctly
+    const user = await User.findById(response.body.user._id)
+    expect(user).not.toBeNull()
+
+    // Assertions about the response
+    expect(response.body).toMatchObject({
+        user: {
+            name: 'Jon',
+            email: 'jon@jon.ca'
+        },
+        token: user.tokens[0].token
+    })
+    expect(user.password).not.toBe('MyPass777!')
+})
+
+//
+// User Test Ideas
+//
+// Should not signup user with invalid name/email/password
+// Should not update user if unauthenticated
+// Should not update user with invalid name/email/password
+// Should not delete user if unauthenticated
