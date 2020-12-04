0da8cb63ade0f5dabacaee2e01300a9619c50b6e
Jonathan Shaw <shaw5@unbc.ca>
2020-12-04 01:43:41 -0800

    altered dependencies so that Heroku can build Vue project 2

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ const app = require('./app')
 const path = require('path')
 
 // If in production, serve up the frontend
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(__dirname, '..', 'pubic')))
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '..', 'pubic')))
 
-//     app.get('*', (req, res) => res.sendFile('../public/index.html'))
-// }
+    app.get('*', (req, res) => res.sendFile('../public/index.html'))
+}
 
 const port = process.env.PORT || 3000
 
