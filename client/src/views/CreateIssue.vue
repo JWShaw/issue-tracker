088f5e043b43a1736bae6eedbb7670c2a5e9fd34
Jonathan Shaw <shaw5@unbc.ca>
2020-12-08 14:56:55 -0800

    Labels can now be added to issues

--- client/src/views/CreateIssue.vue
+++ client/src/views/CreateIssue.vue
@@ -26,6 +26,37 @@
         max-rows="6"
       ></b-form-textarea>
     </b-form-group>
+    <!-- Adapted from the Bootstrap Vue documentation example -->
+    <b-form-group label="Labels:">
+      <b-form-tags v-model="chosenLabels" add-on-change no-outer-focus>
+        <template
+          v-slot="{ tags, inputAttrs, inputHandlers, disabled, removeTag }"
+        >
+          <ul v-if="tags.length > 0" class="list-inline d-inline-block mb-2">
+            <li v-for="tag in tags" :key="tag" class="list-inline-item">
+              <b-form-tag
+                @remove="removeTag(tag)"
+                :title="tag"
+                :disabled="disabled"
+                variant="info"
+                >{{ tag }}</b-form-tag
+              >
+            </li>
+          </ul>
+          <b-form-select
+            v-bind="inputAttrs"
+            v-on="inputHandlers"
+            :disabled="disabled || availableOptions.length === 0"
+            :options="availableOptions"
+          >
+            <template #first>
+              <!-- This is required to prevent bugs with Safari -->
+              <option disabled value="">Choose a label...</option>
+            </template>
+          </b-form-select>
+        </template>
+      </b-form-tags>
+    </b-form-group>
     <b-button type="submit" variant="primary">Create Issue</b-button>
     <b-button variant="danger" href="">Cancel</b-button>
   </b-form>
@@ -38,11 +69,22 @@ export default {
       issue: {
         title: "",
         description: "",
+        labels: [],
       },
+      labelObjects: [],
+      chosenLabels: [],
+      options: [],
     };
   },
   methods: {
     submit() {
+
+      this.labelObjects.forEach((labelObject) => {
+        if (this.chosenLabels.includes(labelObject.name)) {
+          this.issue.labels.push(labelObject._id)
+        }
+      })
+
       this.$http
         .post(
           `/projects/${this.$route.params.projId}/issues`,
@@ -59,6 +101,19 @@ export default {
           return;
         })
         .catch((err) => console.log(err));
+    },
+  },
+  created: async function () {
+    const res2 = await this.$http
+      .get(`/projects/${this.$route.params.projId}/labels`)
+    this.labelObjects = res2.data
+    this.options = this.labelObjects.map(labelObject => labelObject.name)
+  },
+  computed: {
+    availableOptions() {
+      return this.options.filter(
+        (opt) => this.chosenLabels.indexOf(opt) === -1
+      );
     },
   },
 };
--- client/src/views/EditIssue.vue
+++ client/src/views/EditIssue.vue
@@ -26,6 +26,39 @@
         max-rows="6"
       ></b-form-textarea>
     </b-form-group>
+
+    <!-- Adapted from the Bootstrap Vue documentation example -->
+    <b-form-group label="Labels:">
+      <b-form-tags v-model="chosenLabels" add-on-change no-outer-focus>
+        <template
+          v-slot="{ tags, inputAttrs, inputHandlers, disabled, removeTag }"
+        >
+          <ul v-if="tags.length > 0" class="list-inline d-inline-block mb-2">
+            <li v-for="tag in tags" :key="tag" class="list-inline-item">
+              <b-form-tag
+                @remove="removeTag(tag)"
+                :title="tag"
+                :disabled="disabled"
+                variant="info"
+                >{{ tag }}</b-form-tag
+              >
+            </li>
+          </ul>
+          <b-form-select
+            v-bind="inputAttrs"
+            v-on="inputHandlers"
+            :disabled="disabled || availableOptions.length === 0"
+            :options="availableOptions"
+          >
+            <template #first>
+              <!-- This is required to prevent bugs with Safari -->
+              <option disabled value="">Choose a label...</option>
+            </template>
+          </b-form-select>
+        </template>
+      </b-form-tags>
+    </b-form-group>
+
     <b-button type="submit" variant="primary">Update Issue</b-button>
     <b-button
       variant="danger"
@@ -42,11 +75,22 @@ export default {
       issue: {
         title: "",
         description: "",
+        labels: [],
       },
+      labelObjects: [],
+      chosenLabels: [],
+      options: [],
     };
   },
   methods: {
     submit() {
+
+      this.labelObjects.forEach((labelObject) => {
+        if (this.chosenLabels.includes(labelObject.name)) {
+          this.issue.labels.push(labelObject._id)
+        }
+      })
+
       this.$http
         .patch(
           `/projects/${this.$route.params.projId}/issues/${this.$route.params.issueId}`,
@@ -78,6 +122,24 @@ export default {
     );
     this.issue.title = res.data.title;
     this.issue.description = res.data.description;
+
+    const res2 = await this.$http
+      .get(`/projects/${this.$route.params.projId}/labels`)
+    this.labelObjects = res2.data
+    this.options = this.labelObjects.map(labelObject => labelObject.name)
+    // this.chosenLabels = this.labelObjects
+    //   .filter((labelObject) => res.data.labels.includes(labelObject._id))
+    //   .map(labelObject => labelObject.name)
+    // this.options = this.labelObjects
+    //   .map(labelObject => labelObject.name)
+    //   .filter(labelName => !this.chosenLabels.includes(labelName))
+  },
+  computed: {
+    availableOptions() {
+      return this.options.filter(
+        (opt) => this.chosenLabels.indexOf(opt) === -1
+      );
+    },
   },
 };
 </script>
